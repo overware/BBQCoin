019efc2939c2ff1f84487d6998021e56f8ab89ca
K1773R <K1773R@darkgamex.ch>
2013-11-23 21:01:59 +0100

    forgot to include main.h in previous commit

--- src/main.h
+++ src/main.h
@@ -32,7 +32,7 @@ static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const int64 MIN_TX_FEE = 10000000;
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
-static const int64 MAX_MONEY = 4200000000 * COIN; // BBQCoin: maximum of 4200k coins
+static const int64 MAX_MONEY = 201600000 * COIN; // BBQCoin: maximum of 4200k coins
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 10;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
